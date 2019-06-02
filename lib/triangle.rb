959232fa582663ca4b98943ed14da272b4085bf1
Brian Salemi <briandsalemi@gmail.com>
2019-06-02 02:32:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,17 +13,17 @@ class Triangle
       raise TriangleError
     elsif (length_1 + length_2 <= length_3) || (length_1 + length_3 <= length_2) || (length_3 + length_2 <= length_1)
       raise TriangleError
-    elsif 
+    elsif
       (length_1 == length_2) && (length_2 == length_3)
       :equilateral
-    elsif 
-      
+    elsif
+
 
   end
   end
 
   class TriangleError < StandardError
-    
+
 
   end
 end
