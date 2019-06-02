6fca256dd0e6d49631d029abab631c69715a34c4
Brian Salemi <briandsalemi@gmail.com>
2019-06-02 02:16:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,13 @@ class Triangle
     @length_3 = length_3
   end
 
+  def kind
+    if length_1 == length_2 == length_3
+      :equilateral
+    elsif
+
+  end
+
   class TriangleError < StandardError
 
   end
