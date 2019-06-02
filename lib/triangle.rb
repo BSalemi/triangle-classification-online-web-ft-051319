e72105edffc7dd2d9da4392b272a4ea9a51994c2
Brian Salemi <briandsalemi@gmail.com>
2019-06-02 02:14:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end 
+
+  class TriangleError < StandardError
+
+  end 
 end
